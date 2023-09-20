d76edb8d3994af08224c139e584d80acf132fb85
Kaosc <themelihbt@gmail.com>
2023-09-20 12:08:23 +0300

    update

--- _loginInfo.py
+++ _loginInfo.py
@@ -4,11 +4,11 @@ import dotenv
 dotenv.load_dotenv()
 
 # add your username and password to .env file  
-# or replace those with your username and password as string
+# or replace below variables with your username and password as string
 
-# example:
-# IG_USERNAME = "your_username"
-# IG_PASSWORD = "your_password"
+# example .env file content:
+   # IG_USERNAME = "your_username"
+   # IG_PASSWORD = "your_password"
 
-username = os.getenv("IG_USERNAME")
-password = os.getenv("IG_PASSWORD")
+username = os.getenv("IG_USERNAME") # or replace with your username as string. eg: username = "your_username"
+password = os.getenv("IG_PASSWORD") # or replace with your password as string. eg: password = "your_password"
