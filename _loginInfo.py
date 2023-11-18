514c65eb68f9fc081e45fb5d67476cd131af060b
Kaosc <themelihbt@gmail.com>
2023-11-18 23:23:29 +0300

    Fix password & username input validation

--- _loginInfo.py
+++ _loginInfo.py
@@ -3,12 +3,14 @@ import dotenv
 
 dotenv.load_dotenv()
 
-# add your username and password to .env file  
+# add your username and password to .env file
 # or replace below variables with your username and password as string
 
 # example .env file content:
-   # IG_USERNAME = "your_username"
-   # IG_PASSWORD = "your_password"
+# IG_USERNAME = "your_username"
+# IG_PASSWORD = "your_password"
 
-username = os.getenv("IG_USERNAME") # or replace with your username as string. eg: username = "your_username"
-password = os.getenv("IG_PASSWORD") # or replace with your password as string. eg: password = "your_password"
+# or replace with your username as string. eg: username = "your_username"
+username = os.getenv("IG_USERNAME")
+# or replace with your password as string. eg: password = "your_password"
+password = os.getenv("IG_PASSWORD")
